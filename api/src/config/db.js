a17c609e2a40f2a4e207e3a5a431bba51b11ab04
Paulo Henrique Assini <paulohenriqueassini@Mac.localdomain>
2026-01-28 09:46:29 -0300

    Fix: Restaurado import e consolidada conexão direta no db.js

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,13 +1,4 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-console.log('Tentando conexão com o Banco:', {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'dcvb-user',
-    database: process.env.DB_NAME || 'dcvb-db'
-});
 
 const pool = mysql.createPool({
     host: 'localhost',
