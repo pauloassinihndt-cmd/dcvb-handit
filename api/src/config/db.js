c328b17aece440b941e9c8d03ce3653e89220e6d
Paulo Henrique Assini <paulohenriqueassini@Mac.localdomain>
2026-01-28 09:35:03 -0300

    Fix: Credenciais de banco forçadas no código para evitar erro de leitura de caracteres especiais

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -10,10 +10,10 @@ console.log('Tentando conexão com o Banco:', {
 });
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'dcvb-user',
-    password: process.env.DB_PASSWORD || process.env.DB_PASS || 'H4nd1t#DCVB25',
-    database: process.env.DB_NAME || 'dcvb-db',
+    host: 'localhost',
+    user: 'dcvb-user',
+    password: 'H4nd1t#DCVB25',
+    database: 'dcvb-db',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
