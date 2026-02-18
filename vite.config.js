0607aad3b46df100af4291739649fe335a3928c7
Paulo Henrique Assini <paulohenriqueassini@Mac.localdomain>
2026-02-18 16:43:29 -0300

    chore: otimização de build e divisão de chunks

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('jspdf')) return 'jspdf';
+            if (id.includes('html2canvas')) return 'html2canvas';
+            if (id.includes('recharts')) return 'recharts';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
